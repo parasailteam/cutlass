ba513a1d19986e60b0daadbf38d6d48a2cb39d4c
Abhinav Jangda <abhijangda@gmail.com>
2023-02-22 19:17:13 +0000

    u

--- examples/07_volta_tensorop_dependent_gemm/volta_tensorop_gemm.cu
+++ examples/07_volta_tensorop_dependent_gemm/volta_tensorop_gemm.cu
@@ -197,7 +197,7 @@ using SmArch = cutlass::arch::Sm70;
 
 // This code section describes the tile size a thread block will compute
 using ShapeMMAThreadBlock =
-    cutlass::gemm::GemmShape<128, 128, 32>;  // <- threadblock tile M = 128, N = 128, K = 32
+    cutlass::gemm::GemmShape<256, 128, 32>;  // <- threadblock tile M = 128, N = 128, K = 32
 // This code section describes tile size a warp will compute
 using ShapeMMAWarp = cutlass::gemm::GemmShape<64, 64, 32>;  // <- warp tile M = 64, N = 64, K = 32 
 // This code section describes the size of MMA op

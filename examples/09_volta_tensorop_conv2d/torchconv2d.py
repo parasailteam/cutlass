36305017f6685f718564a7ce8bd2a588417050c6
Abhinav Jangda <abhijangda@gmail.com>
2023-04-23 22:05:07 +0000

    different conv sizes

--- examples/09_volta_tensorop_conv2d/torchconv2d.py
+++ examples/09_volta_tensorop_conv2d/torchconv2d.py
@@ -18,6 +18,10 @@ conv2 = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1).cuda()
 conv3 = torch.nn.Conv2d(64, 64, 3, stride=1,padding=1, dtype=torch.float16).cuda()
 conv4 = torch.nn.Conv2d(64, 128, 3, stride=2,padding=1, dtype=torch.float16).cuda()
 conv5 = torch.nn.Conv2d(128, 128, 3, stride=1,padding=1, dtype=torch.float16).cuda()
+conv6 = torch.nn.Conv2d(128, 256, 3, stride=2,padding=1, dtype=torch.float16).cuda()
+conv7 = torch.nn.Conv2d(256, 256, 3, stride=1,padding=1, dtype=torch.float16).cuda()
+conv8 = torch.nn.Conv2d(256, 512, 3, stride=2,padding=1, dtype=torch.float16).cuda()
+conv9 = torch.nn.Conv2d(512, 512, 3, stride=1,padding=1, dtype=torch.float16).cuda()
 
 # conv3 = torch.nn.Conv2d(64, 64, 3, stride=1,padding=1)
 
@@ -25,7 +29,13 @@ conv1_o = conv1(imgs)
 conv2_o = conv2(conv1_o)
 
 conv5_in = conv4(conv2_o)
-print(conv5_in.shape)
+print("Input shape for 128, 3x3", conv5_in.shape)
+
+conv7_in = conv6(conv5_in)
+print("Input shape for 256, 3x3", conv7_in.shape)
+
+conv9_in = conv8(conv7_in)
+print("Input shape for 512, 3x3", conv9_in.shape)
 
 def conv64x64_3(input):
     conv3_o = conv3(input)
@@ -36,6 +46,17 @@ def conv128x128_3(input):
     conv5_o = conv5(input)
     # conv3_o = conv3(conv3_o)
     # conv3_o = conv3(conv3_o)
+
+def conv256x256_3(input):
+    conv7_o = conv7(input)
+    # conv3_o = conv3(conv3_o)
+    # conv3_o = conv3(conv3_o)
+
+def conv512x512_3(input):
+    conv7_o = conv9(input)
+    # conv3_o = conv3(conv3_o)
+    # conv3_o = conv3(conv3_o)
+
 
 for i in range(10):
     conv64x64_3(conv2_o)
@@ -55,3 +76,5 @@ def execute(f, input):
 
 print("3x3 64: ", execute(conv64x64_3, conv2_o))
 print("3x3 128: ", execute(conv128x128_3, conv5_in))
+print("3x3 256: ", execute(conv256x256_3, conv7_in))
+print("3x3 512: ", execute(conv512x512_3, conv9_in))
